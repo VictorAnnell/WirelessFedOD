73cb2a870015e066e376c4850f34bedf3c9a63e2
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2024-05-09 22:44:47 +0200

    Add selection policies based on loss and deviation

--- wireless_fedod/selection_policies.py
+++ wireless_fedod/selection_policies.py
@@ -7,3 +7,17 @@ def random_agent_selection(self, cars):
     if len(clients) == 0:
         clients = cars
     return clients
+
+
+def loss_based_selection(self, cars):
+    """Select half of the clients based on the loss."""
+    clients = sorted(cars, key=lambda x: x.loss, reverse=True)
+    clients = clients[: len(clients) // 2]
+    return clients
+
+
+def deviation_based_selection(self, cars):
+    """Select half of the clients based on the deviation."""
+    clients = sorted(cars, key=lambda x: x.deviation, reverse=True)
+    clients = clients[: len(clients) // 2]
+    return clients
