c3eea209d372f310d5ae6d536d5cd5b6fd1acb11
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2024-05-10 01:48:03 +0200

    Fix type errors in config loader

--- wireless_fedod/config.py
+++ wireless_fedod/config.py
@@ -9,10 +9,15 @@ load_dotenv()
 
 # Dataset configuration
 DATASET_ROOT = os.getenv("DATASET_ROOT", "../datasets")
-DATASET_VERSION = os.getenv("DATASET_VERSION", "mini")
-DATASET_MAX_IMAGES = os.getenv("DATASET_MAX_IMAGES", 200) if DATASET_VERSION == "full" else None
-if DATASET_MAX_IMAGES is not None:
+DATASET_VERSION = os.getenv("DATASET_VERSION", "full")
+DATASET_MAX_IMAGES = os.getenv("DATASET_MAX_IMAGES", 200) if DATASET_VERSION == "full" else 'none'
+try:
     DATASET_MAX_IMAGES = int(DATASET_MAX_IMAGES)
+except ValueError:
+    if DATASET_MAX_IMAGES.lower() == "none":
+        DATASET_MAX_IMAGES = None
+    else:
+        raise ValueError(f"Invalid value for DATASET_MAX_IMAGES: {DATASET_MAX_IMAGES}")
 BATCH_SIZE = int(os.getenv("BATCH_SIZE", 1))
 SHUFFLE_BUFFER_SIZE = int(os.getenv("SHUFFLE_BUFFER_SIZE", BATCH_SIZE * 10))
 OBJECT_CLASSES = OBJECT_CLASSES
@@ -20,7 +25,7 @@ CLASS_MAPPING = dict(zip(range(len(OBJECT_CLASSES)), OBJECT_CLASSES))
 
 # Model configuration
 # Set model creation function:
-MODEL_FN = os.getenv("SELECTION_POLICY", yolov8_model_fn.__name__)  # noqa: F405
+MODEL_FN = os.getenv("MODEL_FN", yolov8_model_fn.__name__)  # noqa: F405
 try:
     MODEL_FN = globals()[MODEL_FN]
 except KeyError:
@@ -29,12 +34,17 @@ except KeyError:
 # Simulator configuration
 NUM_CLIENTS = int(os.getenv("NUM_CLIENTS", 5))
 LOCAL_EPOCHS = int(os.getenv("LOCAL_EPOCHS", 1))
-STEPS_PER_LOCAL_EPOCH = os.getenv("STEPS_PER_LOCAL_EPOCH", None)
-if STEPS_PER_LOCAL_EPOCH is not None:
+STEPS_PER_LOCAL_EPOCH = os.getenv("STEPS_PER_LOCAL_EPOCH", 'none')
+try:
     STEPS_PER_LOCAL_EPOCH = int(STEPS_PER_LOCAL_EPOCH)
+except ValueError:
+    if STEPS_PER_LOCAL_EPOCH.lower() == "none":
+        STEPS_PER_LOCAL_EPOCH = None
+    else:
+        raise ValueError(f"Invalid value for STEPS_PER_LOCAL_EPOCH: {STEPS_PER_LOCAL_EPOCH}")
 SIMULATION_ID = os.getenv("SIMULATION_ID", None)
 # Set agent selection policy function:
-AGENT_SELECTION_FN = os.getenv("SELECTION_POLICY", "random_agent_selection")
+AGENT_SELECTION_FN = os.getenv("AGENT_SELECTION_FN", "random_agent_selection")
 try:
     AGENT_SELECTION_FN = globals()[AGENT_SELECTION_FN]
 except KeyError:
--- wireless_fedod/main.py
+++ wireless_fedod/main.py
@@ -9,10 +9,7 @@ load_dotenv()
 
 def test_policies():
     # Load ZOD dataset
-    zod_train, zod_test = load_zod(
-        # version="mini",
-        version="full",
-    )
+    zod_train, zod_test = load_zod()
     for policy in [random_agent_selection, loss_based_selection, deviation_based_selection]:
         simulator = WirelessFedODSimulator(num_clients=10, simulation_id=policy.__name__)
         simulator.train_data = zod_train
@@ -27,17 +24,14 @@ if __name__ == "__main__":
     simulator = WirelessFedODSimulator(num_clients=5)
 
     # Load ZOD dataset
-    zod_train, zod_test = load_zod(
-        # version="mini",
-        version="full",
-    )
+    zod_train, zod_test = load_zod()
 
     # Set dataset, model_fn, and agent_selection_fn
     simulator.train_data = zod_train
     simulator.test_data = zod_test
-    simulator.model_fn = yolov8_model_fn
-    simulator.num_clients = 1  # For testing
-    simulator.local_epochs = 1  # For testing
-    simulator.steps_per_local_epoch = 1  # For testing
+    # simulator.model_fn = yolov8_model_fn
+    # simulator.num_clients = 1  # For testing
+    # simulator.local_epochs = 1  # For testing
+    # simulator.steps_per_local_epoch = 1  # For testing
     # while simulator.metrics['loss'] > 1:
     simulator.run_round()
