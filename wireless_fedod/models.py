29fdb7b58bc66623d0c8ebab4e75c24ea5d106ed
VictorAnnell <30280491+VictorAnnell@users.noreply.github.com>
2024-06-13 05:34:19 +0200

    Use custom parameters for YOLO prediction decoder

--- wireless_fedod/models.py
+++ wireless_fedod/models.py
@@ -1,20 +1,27 @@
 import keras
 import keras_cv
 
+yolo_prediction_decoder = keras_cv.layers.NonMaxSuppression(
+    bounding_box_format="xyxy",
+    from_logits=True,
+    # Decrease the required threshold to make predictions get pruned out
+    iou_threshold=0.6,
+    # Tune confidence threshold for predictions to pass NMS
+    confidence_threshold=0.51,
+)
+
 
 def yolov8xs_model_fn() -> keras.Model:
     from wireless_fedod.config import OBJECT_CLASSES
 
-    model = keras_cv.models.YOLOV8Detector(
+    model = keras_cv.models.YOLOV8Detector.from_preset(
+        "yolo_v8_xs_backbone",
         num_classes=len(OBJECT_CLASSES),
         bounding_box_format="xyxy",
-        backbone=keras_cv.models.YOLOV8Backbone.from_preset("yolo_v8_xs_backbone", load_weights=False),
         fpn_depth=2,
+        prediction_decoder=yolo_prediction_decoder,
     )
-    base_lr = 0.005
-    # including a global_clipnorm is extremely important in object detection tasks
-    # optimizer = keras.optimizers.SGD(learning_rate=base_lr, momentum=0.9, global_clipnorm=10.0)
-    optimizer = keras.optimizers.Adam(learning_rate=base_lr, global_clipnorm=10.0)
+    optimizer = keras.optimizers.Adam(learning_rate=0.005, global_clipnorm=10.0)
     model.compile(
         classification_loss="binary_crossentropy",
         box_loss="ciou",
@@ -27,13 +34,14 @@ def yolov8xs_model_fn() -> keras.Model:
 def yolov8xs_coco_model_fn() -> keras.Model:
     from wireless_fedod.config import OBJECT_CLASSES
 
-    base_model = keras_cv.models.YOLOV8Detector(
+    base_model = keras_cv.models.YOLOV8Detector.from_preset(
+        "yolo_v8_xs_backbone_coco",
         num_classes=len(OBJECT_CLASSES),
         bounding_box_format="xyxy",
-        backbone=keras_cv.models.YOLOV8Backbone.from_preset("yolo_v8_xs_backbone_coco"),
         fpn_depth=2,
+        prediction_decoder=yolo_prediction_decoder,
     )
-    optimizer = keras.optimizers.SGD(learning_rate=0.005, momentum=0.9, global_clipnorm=10.0)
+    optimizer = keras.optimizers.Adam(learning_rate=0.005, global_clipnorm=10.0)
     base_model.compile(
         classification_loss="binary_crossentropy",
         box_loss="ciou",
@@ -41,29 +49,27 @@ def yolov8xs_coco_model_fn() -> keras.Model:
         jit_compile=False,
     )
     return base_model
+
 
 def yolov8s_coco_model_fn() -> keras.Model:
     from wireless_fedod.config import OBJECT_CLASSES
 
-    base_model = keras_cv.models.YOLOV8Detector(
+    model = keras_cv.models.YOLOV8Detector.from_preset(
+        "yolo_v8_s_backbone_coco",
         num_classes=len(OBJECT_CLASSES),
         bounding_box_format="xyxy",
-        backbone=keras_cv.models.YOLOV8Backbone.from_preset("yolo_v8_s_backbone_coco"),
         fpn_depth=1,
+        prediction_decoder=yolo_prediction_decoder,
     )
-    optimizer = keras.optimizers.SGD(learning_rate=0.005, momentum=0.9, global_clipnorm=10.0)
-    # optimizer = keras.optimizers.Adam(learning_rate=0.001, global_clipnorm=10.0)
-    # boundaries = [100000, 110000]
-    # values = [1.0, 0.5, 0.1]
-    # optimizer = keras.optimizers.schedules.PiecewiseConstantDecay(
-    #     boundaries, values)
-    base_model.compile(
+    optimizer = keras.optimizers.Adam(learning_rate=0.005, global_clipnorm=10.0)
+    model.compile(
         classification_loss="binary_crossentropy",
         box_loss="ciou",
         optimizer=optimizer,
         jit_compile=False,
     )
-    return base_model
+    return model
+
 
 def retinanet_resnet50_model_fn() -> keras.Model:
     from wireless_fedod.config import OBJECT_CLASSES
